5ea3782df1921b30845d93007b6143e491d32b52
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-03-25 12:09:15 +0500

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,13 @@
 import Marquee3k from 'marquee3000';
 Marquee3k.init()
+
+// import Swiper from 'swiper';
+// import 'swiper/css';
+
+// const reviewsSlider = new Swiper('.reviews-slider', {
+// 	slidesPerView: 2.5,
+// 	spaceBetween: 37,
+// 	loop: true,
+// 	speed: 1000,
+// 	// centeredSlides: true,
+// });
